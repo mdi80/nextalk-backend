dfcd2e3b71f7e01ab1bc28c4ee13b9bc2d8fd6c2
mdi80 <mdi80nz@gmail.com>
2023-09-07 01:48:40 +0330

    update

--- nextalk/nextalk/asgi.py
+++ nextalk/nextalk/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nextalk.settings")
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
@@ -17,13 +18,12 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
 from chat.auth import TokenAuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nextalk.settings")
-
+asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            TokenAuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
 )
