3346f376ecf0e809834e88da61c6df50fdf62bd4
mdi80 <mdi80nz@gmail.com>
2023-09-07 16:28:08 +0330

    test2

--- nextalk/nextalk/asgi.py
+++ nextalk/nextalk/asgi.py
@@ -13,6 +13,7 @@ import django
 django.setup()
 
 
+
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
