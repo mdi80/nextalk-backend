70b42ddf7bb98fa99cbeab994b48468e85fac742
mdi80 <mdi80nz@gmail.com>
2023-09-07 16:24:56 +0330

    test

--- nextalk/nextalk/asgi.py
+++ nextalk/nextalk/asgi.py
@@ -12,6 +12,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nextalk.settings")
 import django
 django.setup()
 
+
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
